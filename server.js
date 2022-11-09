f640bdb925bfc622ad80dc8570f7ee5e5f7927a9
saiteja-107 <160120737107kotlasaiteja@gmail.com>
2022-11-09 21:05:51 +0530

    Edited

--- server.js
+++ server.js
@@ -4,21 +4,24 @@ const app=express();
 app.use(bodyParser.urlencoded({extended:true}))
 const mongoose=require("mongoose");
 const { urlencoded, json } = require("body-parser");
-
-const db="mongodb+srv://saiteja107:rsbWEZoph8fwRszv@cluster0.pupwcw8.mongodb.net/Users?retryWrites=true&w=majority"
+const db="mongodb+srv://saiteja107:rsbWEZoph8fwRszv@cluster0.pupwcw8.mongodb.net/waste?retryWrites=true&w=majority"
 mongoose.connect(db,{useNewUrlParser:true,useUnifiedTopology:true}).then(()=>{
     console.log("Connectiojn success ")
 }).catch((err)=>{
     console.log(err);})
 
+
 const userSchema=new mongoose.Schema({
     email:String,
     password:String,
 })
+
+
 const user=mongoose.model("details",userSchema);
 app.get("/home",(req,res)=>{
 res.sendFile(__dirname+"/index.html")
 })
+
 
 app.post("/update",(req,res)=>{
     const user1=new user({email:req.body.email,password:req.body.password}).save().then(()=>{
@@ -30,9 +33,12 @@ app.post("/update",(req,res)=>{
 
 })
 
+
 app.get("/details",(req,res)=>{
     const d= user.findOne().then((data)=>{
+
         res.send(data)
+
     });
     // res.send(` Email is  : ${d.email}  password : ${d.password}`);
 })
